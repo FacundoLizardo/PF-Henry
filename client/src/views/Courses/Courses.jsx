b73d76006ee4d6654e5e56b5b850f9911ee3624f
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-26 14:33:23 -0300

    Cambios generales en el Front End

--- client/src/views/Courses/Courses.jsx
+++ client/src/views/Courses/Courses.jsx
@@ -1,16 +1,41 @@
+import CardContainer from "../../Components/CardContainer/CardContainer";
 import Filters from "../../Components/Filters/Filters";
 import { listCourses } from "../../utils/data";
+import { useNavigate } from "react-router-dom";
 
 import Styles from "./Courses.module.css";
 
 const Courses = () => {
+  const navigate = useNavigate();
+
+  const groupedCourses = listCourses.reduce((result, course) => {
+    if (!result[course.category]) {
+      result[course.category] = [];
+    }
+    result[course.category].push(course);
+    return result;
+  }, {});
+
+  const handleNavigate = (detailId) => {
+    navigate(`./detailCourse/${detailId}`);
+  };
+
   return (
     <div className={Styles.coursesContainer}>
       <Filters />
       <div>
-        {listCourses.map((course, index) => (
-          <div key={index}>
-            <p>{course.category}</p>
+        {Object.entries(groupedCourses).map(([category, courses], index) => (
+          <div className={Styles.categoryContainer} key={index}>
+            <h2>{category}</h2>
+            <div className={Styles.courses}>
+              {courses.map((course, courseIndex) => (
+                <CardContainer
+                  course={course}
+                  key={courseIndex}
+                  handleNavigate={handleNavigate}
+                />
+              ))}
+            </div>
           </div>
         ))}
       </div>
--- client/src/views/Layout/Layout.jsx
+++ client/src/views/Layout/Layout.jsx
@@ -1,83 +1,14 @@
-import Button from "../../Components/Button/Button";
-import CardContainer from "../../Components/CardContainer/CardContainer";
 import Footer from "../../Components/Footer/Footer";
 import Home from "../../Components/Home/Home";
 import { Link } from "react-router-dom";
 import Styles from "./Layout.module.css";
 
 const Layout = () => {
-	const course = [
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-		{
-			title: "Curso de microlearning",
-			description: "aqui aprenderas que es el microlearning",
-			rating: 4,
-			image:
-				"https://i.ytimg.com/vi/MHip3eVhK9g/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBa2Tc3wl_CWfJzbvhBmHo1sBXdEg",
-			instructorImage:
-				"https://yt3.ggpht.com/peu87JRecAO0qd7KhfPHLrQ_XJBEiuAiNHGuGU74dvJRnfNzP6x3sNfiIINRISZpDIqQzRgFpg=s68-c-k-c0x00ffffff-no-rj",
-		},
-	];
 	return (
 		<div className={Styles.layoutContainer}>
 			<Home />
 			<div className={Styles.layCoutontent}>
 				<h2>Cursos mejor valorados por nuestros alumnos</h2>
-
-				<CardContainer course={course} />
-				<h2>Categorias</h2>
-				<CardContainer course={course} />
-				<h2>Ultimos cursos </h2>
-				<CardContainer course={course} />
-				<h2>Novedades</h2>
-
 				<Link to="/courses">
 					<button className={Styles.buttonCourses}>Ver todos cursos</button>
 				</Link>
