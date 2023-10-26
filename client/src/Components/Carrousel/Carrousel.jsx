0ce957f89dce66649a3b386fc594d6d31704a7ed
kayitaC2024 <kayita_y@hotmail.com>
2023-10-26 18:24:35 -0500

    carousel con las imagenes

--- client/src/Components/Carrousel/Carrousel.jsx
+++ client/src/Components/Carrousel/Carrousel.jsx
@@ -1,21 +1,37 @@
 import { Carousel } from "react-responsive-carousel";
 import style from "./ProjectsCarrousel.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import image1 from "../../assets/imgCarrousel/img1.jpg";
 import image2 from "../../assets/imgCarrousel/img2.jpg";
+import image3 from "../../assets/imgCarrousel/img3.jpg";
+import image4 from "../../assets/imgCarrousel/img4.jpg";
+import image5 from "../../assets/imgCarrousel/img5.jpg";
 
-const objSlides = [{ image: image1 }, { image: image2 }];
+
+
+const objSlides = [
+  { image: image1 },
+  { image: image2 },
+  { image: image3 },
+  { image: image4 },
+  { image: image5 }
+];
 
 export const Carrousel = () => {
   return (
+
     <div id="projects" className={style.container}>
-      <Carousel showStatus={false} animationHandler="fade">
+      <Carousel showStatus={false} animationHandler="fade" autoPlay={true} interval={2000} infiniteLoop={true} showThumbs={false}>
         {objSlides?.map((objSlide, index) => (
           <div key={index}>
-            <img src={objSlide.image} />
+            <img src={objSlide.image} alt={`Image ${index}`} />
           </div>
         ))}
       </Carousel>
     </div>
   );
 };
+
+
+
