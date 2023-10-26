7c6364c759d5fd956b98c700639265e5781a59df
FacundoLizardo <facundolizardo75@gmail.com>
2023-10-26 15:37:34 -0300

    vista login

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const { conn } = require("./src/db.js");
 // const { } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {	
-  server.listen(3001, () => {
-    console.log(`%Server listening at ${port}`);
-  });
+conn.sync({ force: false }).then(() => {
+	server.listen(3001, () => {
+		console.log(`%Server listening at ${port}`);
+	});
 });
---
+++ client/src/views/Login/Login.jsx
@@ -0,0 +1,11 @@
+import Styles from "./Login.module.css";
+
+const Login = () => {
+	return (
+		<div className={Styles.loginContainer}>
+			<h2>login</h2>
+		</div>
+	);
+};
+
+export default Login;
