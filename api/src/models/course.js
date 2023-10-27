4c2244ea2f968599357cd53a1d64eb7730502e04
JoseValperga <jose.valperga@gmail.com>
2023-10-27 14:48:33 -0300

    Rutas get cursos x nombres y por id

--- api/src/models/course.js
+++ api/src/models/course.js
@@ -1,51 +1,51 @@
 const { DataTypes } = require("sequelize");
 
 const Course = (sequelize) => {
-	sequelize.define(
-		"Course",
-		{
-			id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				primaryKey: true,
-				allowNull: false,
-			},
-			title: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			description: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			instructor_id: {
-				type: DataTypes.UUID,
-				allowNull: true,
-			},
-			progress: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				defaultValue: 0,
-			},
-			category: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-			image: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			deletedAt: {
-				type: DataTypes.DATE,
-				allowNull: true,
-			},
-		},
+  sequelize.define(
+    "Course",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      instructor_id: {
+        type: DataTypes.UUID,
+        allowNull: true,
+      },
+      progress: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
 
-		{
-			timestamps: true,
-			paranoid: true,
-		}
-	);
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
 };
 
 module.exports = Course;
--- api/src/models/rating.js
+++ api/src/models/rating.js
@@ -1,43 +1,43 @@
 const { DataTypes } = require("sequelize");
 
 const Rating = (sequelize) => {
-	sequelize.define(
-		"Rating",
-		{
-			id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				primaryKey: true,
-				allowNull: false,
-			},
-			course_id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				allowNull: false,
-			},
-			user_id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				allowNull: false,
-			},
-			rating: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-			comment: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			deletedAt: {
-				type: DataTypes.DATE,
-				allowNull: true, // Para permitir registros no eliminados
-			},
-		},
-		{
-			timestamps: true,
-			paranoid: true,
-		}
-	);
+  sequelize.define(
+    "Rating",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      course_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true, // Para permitir registros no eliminados
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
 };
 
 module.exports = Rating;
