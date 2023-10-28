c244c30cf8a86abfeac0f4a30b2d51a787e64d78
FacundoLizardo <facundolizardo75@gmail.com>
2023-10-28 11:13:04 -0300

    Renderizando courses dentro de componente filter

--- client/src/views/Courses/Courses.jsx
+++ client/src/views/Courses/Courses.jsx
@@ -1,40 +1,33 @@
 import CardContainer from "../../Components/CardContainer/CardContainer";
-import Filters from "../../Components/Filters/Filters";
-import { useEffect, useState } from "react";
-
 import Styles from "./Courses.module.css";
 
-const Courses = () => {
-  const [dataCourses, setDataCourses] = useState([]);
-  useEffect(() => {
-    setDataCourses(JSON.parse(localStorage.getItem("coursesData")));
-  }, []);
+const Courses = ({ dataCourses }) => {
+	const groupedCourses = dataCourses.reduce((result, course) => {
+		if (!result[course.category]) {
+			result[course.category] = [];
+		}
+		result[course.category].push(course);
+		return result;
+	}, {});
 
-  const groupedCourses = dataCourses.reduce((result, course) => {
-    if (!result[course.category]) {
-      result[course.category] = [];
-    }
-    result[course.category].push(course);
-    return result;
-  }, {});
-
-  return (
-    <div className={Styles.coursesContainer}>
-      <Filters />
-      <div>
-        {Object.entries(groupedCourses).map(([category, courses], index) => (
-          <div className={Styles.categoryContainer} key={index}>
-            <h2>{category}</h2>
-            <div className={Styles.courses}>
-              {courses.map((course, courseIndex) => (
-                <CardContainer course={course} key={courseIndex} />
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className={Styles.mainContainer}>
+			<div className={Styles.coursesContainer}>
+				<div>
+					{Object.entries(groupedCourses).map(([category, courses], index) => (
+						<div className={Styles.categoryContainer} key={index}>
+							<h2>{category}</h2>
+							<div className={Styles.courses}>
+								{courses.map((course, courseIndex) => (
+									<CardContainer course={course} key={courseIndex} />
+								))}
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Courses;
