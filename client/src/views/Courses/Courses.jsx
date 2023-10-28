67cfbd9f7c531f9c22b275161aae2429da7c6283
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-28 16:36:53 -0300

    Se agrega icono de desplazamiento a las Categorias

--- client/src/views/Courses/Courses.jsx
+++ client/src/views/Courses/Courses.jsx
@@ -25,9 +25,36 @@ const Courses = () => {
       <div>
         {Object.entries(groupedCourses).map(([category, courses], index) => (
           <div className={Styles.categoryContainer} key={index}>
-            <h2 onClick={() => setActiveCategory(activeCategory === category ? null : category)}
-              className={Styles.categoryHeader}>{category}</h2>
-            <div className={`${Styles.courses} ${activeCategory !== category ? Styles.hidden : ""}`}>
+            <h2
+              onClick={() =>
+                setActiveCategory(activeCategory === category ? null : category)
+              }
+              className={Styles.categoryHeader}
+            >
+              {category}{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                fill="none"
+                className={
+                  activeCategory === category
+                    ? Styles.activeCategorySVG
+                    : Styles.inactiveCategorySVG
+                }
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15 11l-3 3l-3 -3"></path>
+                <path d="M12 3a9 9 0 1 0 0 18a9 9 0 0 0 0 -18z"></path>
+              </svg>
+            </h2>
+            <div
+              className={`${Styles.courses} ${
+                activeCategory !== category ? Styles.hidden : ""
+              }`}
+            >
               {courses.map((course, courseIndex) => (
                 <CardContainer course={course} key={courseIndex} />
               ))}
