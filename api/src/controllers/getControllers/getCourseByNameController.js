f3cb1d9c0c9cc88492f67b70b6ddebfa4fcf4dcd
JoseValperga <jose.valperga@gmail.com>
2023-10-27 21:18:55 -0300

    Va con FilterBC.jsx (eso espero)

--- api/src/controllers/getControllers/getCourseByNameController.js
+++ api/src/controllers/getControllers/getCourseByNameController.js
@@ -16,20 +16,3 @@ const getCourseByNameController = async (propiedad, valorPropiedad) => {
 };
 
 module.exports = { getCourseByNameController };
-
-/*const getCourseByNameController = async (title) => {
-  const courses = await Course.findAll({
-    where: {
-      title: { [Op.iLike]: `%${title}%` },
-      deletedAt: null,
-    },
-    include: { model: Lesson, as: "lesson" },
-  });
-
-  return courses.map((elemento) => {
-    return elemento.dataValues;
-  });
-};
-
-module.exports = { getCourseByNameController };
- */
--- client/src/Components/Filters/FiltersBc.jsx
+++ client/src/Components/Filters/FiltersBc.jsx
@@ -1,26 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const filterBack = async (params = "") => {
+export const filterBack = async (params = "/") => {
   try {
     const data = response.data;
     const response = await axios.get(`/courses?${params.type}=${params.value}`);
 
-    // Actualiza los datos de cursos en localStorage
-    localStorage.setItem(`${params.type}Courses`, JSON.stringify(data));
+    if (params !== "/") {
+      localStorage.setItem(`${params.type}Courses`, JSON.stringify(data));
+    }
   } catch (error) {
-    console.log('Error en el front', error);
+    console.log("Error en el front", error);
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
