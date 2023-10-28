fdfbbd120a9cf7fc1138a32237cd7f0f8e524e2e
FacundoLizardo <facundolizardo75@gmail.com>
2023-10-28 04:04:31 -0300

    filtros por categoria andando, falta por rating y orden por precio

--- client/src/Components/Filters/Filters.jsx
+++ client/src/Components/Filters/Filters.jsx
@@ -1,115 +1,123 @@
-import { useRef } from 'react';
-import Styles from './Filters.module.css';
+import Styles from "./Filters.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import Courses from "../../views/Courses/Courses";
 
-const categories = [
-    "Desarrollo web",
-    "Marketing digital",
-    "Programación",
-    "Diseño gráfico",
-    "Idiomas",
-];
+export default function Filters() {
+	const [categoriesData, setCategoriesData] = useState([]);
+	const [categoryFilter, setCategoryFilter] = useState("undefined");
+	const [ratingFilter, setRatingFilter] = useState("undefined");
+	const [order, setOrder] = useState("udefined");
 
-export default function Filters({ updateFilters, handleResetFilter }) {
-  const filterCategory = useRef(null);
-  const orderName = useRef(null);
-  const orderPopularity = useRef(null);
-  const filterRating = useRef(null);
+	const [dataCourses, setDataCourses] = useState({
+		data: [],
+		filteredData: [],
+	});
 
-  
-  const handleFilterChange = (fieldName, selectedValue) => {
-    if (fieldName === 'rating') {
-      const rating = selectedValue !== 'undefined' ? parseInt(selectedValue, 10) : undefined;
-      updateFilters(fieldName, rating);
-    } else {
-      updateFilters(fieldName, selectedValue);
-    }
-  
-    // Restablecer filtros relacionados
-    if (fieldName === 'order' && selectedValue !== 'undefined') {
-      orderPopularity.current.value = 'undefined';
-    }
-    if (fieldName === 'popularity' && selectedValue !== 'undefined') {
-      orderName.current.value = 'undefined';
-    }
-  };
-  
+	// setState((prevState) => ({
+	// 	...prevState,
+	// 	rating: 5, // El nuevo valor de "rating"
+	// }));
 
-  // Resetear todos los filtros
-  const handleReset = () => {
-    filterCategory.current.value = 'undefined';
-    orderName.current.value = 'undefined';
-    orderPopularity.current.value = 'undefined';
-    filterRating.current.value = 'undefined';
-    handleResetFilter();
-  };
+	useEffect(() => {
+		setDataCourses((prevState) => ({
+			...prevState,
+			data: JSON.parse(localStorage.getItem("coursesData")),
+		}));
+		setCategoriesData(JSON.parse(localStorage.getItem("categoriesData")));
+	}, []);
 
-  return (
-    <section id="container" className={Styles.filtersContainer}>
-      <div className={Styles.filtOrderCont}>
-        <div className={Styles.filters}>
-          <select
-            className={Styles.filterButt}
-            ref={orderName}
-            onChange={(e) => handleFilterChange('order', e.target.value)}
-            defaultValue="undefined"
-            
-          >
-            </select>
-          
-          <select
-            className={Styles.filterButt}
-            ref={filterCategory}
-            onChange={(e) => handleFilterChange('category', e.target.value)}
-            defaultValue="undefined"
-          >
-            <option value="undefined">Categorías</option>
-            {categories.length > 0 && categories.map((category, index) => (
-                    <option key={index} value={category}>{category}</option>
-                ))}
-          </select>
-          <select
-            className={Styles.filterButt}
-            ref={filterRating}
-            onChange={(e) => handleFilterChange('rating', e.target.value)}
-            defaultValue="undefined"
-          >
-            <option value="undefined" disabled>
-              Filtrar por rating
-            </option>
-            <option value="5">5 estrellas</option>
-            <option value="4">4 estrellas</option>
-            <option value="3">3 estrellas</option>
-            <option value="2">2 estrellas</option>
-            <option value="1">1 estrella</option>
-          </select>
-          <select
-          
-            className={Styles.filterButt}
-            ref={orderPopularity}
-            onChange={(e) => handleFilterChange('price', e.target.value)}
-            defaultValue="undefined"
-          >
-            <option value="undefined" disabled>
-            Ordenar por popularidad
-            </option>
-            <option value="ASC">Menor a mayor</option>
-            <option value="DESC">Mayor a menor</option>
-          </select>
-        </div>
+	const handleChange = (event) => {
+		event.preventDefault();
+		const { name, value } = event.target;
+		if (name === "category") {
+			filterCourses(value);
+			setCategoryFilter(value);
+		}
+	};
 
-        <div className={Styles.reset}>
-          <button
-           type='submit' 
-            className="reset-button"
-            onClick={handleReset}
-          >
-            Restablecer
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+	const filterCourses = (value) => {
+		setDataCourses((prevState) => ({
+			...prevState,
+			filteredData: dataCourses.data.filter((course) => {
+				return course.category === value;
+			}),
+		}));
+	};
+
+	const handleChangeOrder = () => {};
+
+	const handleReset = () => {
+		setDataCourses((prevState) => ({
+			...prevState,
+			filteredData: [],
+		}));
+	};
+
+	return (
+		<div className={Styles.mainContainer}>
+			<section id="container" className={Styles.filtersContainer}>
+				<div className={Styles.filtOrderCont}>
+					<div className={Styles.filters}>
+						{/* 		
+
+ */}
+						<select
+							className={Styles.filterButt}
+							onChange={handleChange}
+							name="category"
+							value={categoryFilter}
+						>
+							<option>Categorías</option>
+							{categoriesData?.map((category, index) => (
+								<option key={index}>{category.name}</option>
+							))}
+						</select>
+						{/* 		
+
+
+ */}
+						<select
+							className={Styles.filterButt}
+							onChange={handleChange}
+							value={ratingFilter}
+							name="rating"
+						>
+							<option disabled>Filtrar por rating</option>
+							<option value="5">5 estrellas</option>
+							<option value="4">4 estrellas</option>
+							<option value="3">3 estrellas</option>
+							<option value="2">2 estrellas</option>
+							<option value="1">1 estrella</option>
+						</select>
+
+						{/* 		
+
+
+ */}
+						<select
+							className={Styles.filterButt}
+							value={order}
+							onChange={handleChangeOrder}
+						>
+							<option disabled>Ordenar por precio</option>
+							<option value="ASC">Menor a mayor</option>
+							<option value="DESC">Mayor a menor</option>
+						</select>
+					</div>
+
+					<div className={Styles.reset}>
+						<button className="reset-button" onClick={handleReset}>
+							Restablecer
+						</button>
+					</div>
+				</div>
+			</section>
+			{dataCourses.filteredData.length >= 1 ? (
+				<Courses dataCourses={dataCourses.filteredData} />
+			) : (
+				<Courses dataCourses={dataCourses.data} />
+			)}
+		</div>
+	);
 }
-
-
-
