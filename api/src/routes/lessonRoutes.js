79da0f46b0b61d76fb6d07346c6ece92d189b762
JoseValperga <jose.valperga@gmail.com>
2023-11-02 17:39:11 -0300

    Ruta busqueda lesson por Id lista

--- api/src/routes/lessonRoutes.js
+++ api/src/routes/lessonRoutes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const { postHandlerLessons } = require("../handlers/postHandlerLessons");
-const { getHandlerLessonsByCourseId } = require("../handlers/getHandlerLessonsByCourseId");
+const {
+  getHandlerLessonsByCourseId,
+} = require("../handlers/getHandlerLessonsByCourseId");
+const { getHandlerLessonsById } = require("../handlers/getHandlerLessonsById");
 
 const lessonRoutes = Router();
 
 lessonRoutes.post("/create", postHandlerLessons);
 lessonRoutes.get("/:course_id", getHandlerLessonsByCourseId);
+lessonRoutes.get("/lesson/:id", getHandlerLessonsById);
 
 module.exports = lessonRoutes;
