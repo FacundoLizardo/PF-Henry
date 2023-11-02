5b57e758eb274225fd18aca571f7b980380807e4
JoseValperga <jose.valperga@gmail.com>
2023-11-02 13:37:16 -0300

    Listo get de Lessons por id curso

--- api/src/handlers/getHandlerLessonsByCourseId.js
+++ api/src/handlers/getHandlerLessonsByCourseId.js
@@ -1,3 +1,22 @@
+const {
+  getCourseByIdController,
+} = require("../controllers/getControllers/getCourseByIdController");
+
+const getHandlerLessonsByCourseId = async (req, res) => {
+  const { course_id } = req.params;
+  try {
+    const lessons = await getCourseByIdController(course_id);
+    return res.status(200).json(lessons.lesson);
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: error.message + " El curso no existe" });
+  }
+};
+
+module.exports = { getHandlerLessonsByCourseId };
+
+/*
 const getLessonsByCourseIdController = require("../controllers/getControllers/getLessonsByCourseIdController");
 
 const getHandlerLessonsByCourseId = async (req, res) => {
@@ -13,3 +32,4 @@ const getHandlerLessonsByCourseId = async (req, res) => {
 };
 
 module.exports = { getHandlerLessonsByCourseId };
+*/
