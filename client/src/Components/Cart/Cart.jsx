2a970e2c7dde905f4da52cd792ba78a4cc5cc111
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-11-02 12:02:18 -0300

    Diseño de carrito, navbar y detalles en la navegación

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,65 +1,116 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useCart } from "../../context/CartContext";
+import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
+import { userContext } from "../../App";
 import Styles from "./Cart.module.css";
 
 function Cart() {
+  const userData = useContext(userContext);
   const { state, dispatch } = useCart();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const navigate = useNavigate();
+  let timer;
+
+  const handleNavigate = () => {
+    navigate(`./cart/${userData.id}`);
+  };
 
   const handleRemoveFromCart = (productId) => {
     dispatch({ type: "REMOVE_FROM_CART", payload: productId });
   };
 
   const openModal = () => {
+    clearTimeout(timer);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
-    setIsModalOpen(false);
+    timer = setTimeout(() => {
+      setIsModalOpen(false);
+    }, 1000);
   };
 
+  const handleMouseEnter = () => {
+    openModal();
+  };
+
+  const handleMouseLeave = () => {
+    closeModal();
+  };
+
+  useEffect(() => {
+    const total = state.cart.reduce((acc, product) => acc + product.price, 0);
+    setTotalPrice(total);
+  }, [state.cart]);
+
+  const isCartEmpty = state.cart.length === 0;
+
   return (
-    <div>
-      <ul>
-        {state.cart.map((product, index) => (
-          <li key={index} className={Styles.cartItem}>
-            {product.name} - ${product.price}
-            <button
-              className={Styles.cartRemoveItem}
-              onClick={() => handleRemoveFromCart(product.id)}
-            >
-              Eliminar
-            </button>
-          </li>
-        ))}
-      </ul>
-      {state.cart.length > 0 && (
-        <div>
-          <button className={Styles.cartBuyItem} onClick={openModal}>
-            Comprar
-          </button>
-          {isModalOpen && (
-            <div className={Styles.modal}>
-              <div className={Styles.modalContent}>
-                <span className="close-button" onClick={closeModal}>
-                  &times;
-                </span>
-                <h2>Resumen de compra</h2>
-                <ul>
-                  {state.cart.map((product, index) => (
-                    <li key={index} className={Styles.cartItem}>
-                      {product.name} - ${product.price}
-                    </li>
-                  ))}
-                </ul>
-                <button className={Styles.cartBuyItem} onClick={closeModal}>
-                  Confirmar Compra
-                </button>
-              </div>
-            </div>
-          )}
+    <div
+      className={Styles.cartContainer}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {isModalOpen && (
+        <div
+          className={Styles.modal}
+          onMouseEnter={openModal}
+          onMouseLeave={closeModal}
+        >
+          <div className={Styles.modalContent}>
+            {isCartEmpty ? (
+              <p>El carrito está vacío</p>
+            ) : (
+              <ul>
+                {state.cart.map((product, index) => (
+                  <li key={index}>
+                    <div>
+                      <p>{product.name}</p>
+                      <span>US$ {product.price}</span>
+                    </div>
+                    <button
+                      className={Styles.cartRemoveItem}
+                      onClick={() => handleRemoveFromCart(product.id)}
+                    >
+                      Eliminar
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {!isCartEmpty && (
+              <div className={Styles.totalPrice}>Total: US$ {totalPrice}</div>
+            )}
+            {!isCartEmpty && (
+              <Button
+                className={Styles.cartBuyItem}
+                text={"Comprar"}
+                onClick={() => {
+                  closeModal();
+                  handleNavigate();
+                }}
+              />
+            )}
+          </div>
         </div>
       )}
+
+      <button className={Styles.cartButton}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="128"
+          height="128"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="currentColor"
+            d="M14 13.1V12H4.6l.6-1.1l9.2-.9L16 4H3.7L3 1H0v1h2.2l2.1 8.4L3 13v1.5c0 .8.7 1.5 1.5 1.5S6 15.3 6 14.5S5.3 13 4.5 13H12v1.5c0 .8.7 1.5 1.5 1.5s1.5-.7 1.5-1.5c0-.7-.4-1.2-1-1.4zM4 5h10.7l-1.1 4l-8.4.9L4 5z"
+          />
+        </svg>{" "}
+        <span className={Styles.cartLength}>{state.cart.length}</span>
+      </button>
     </div>
   );
 }
