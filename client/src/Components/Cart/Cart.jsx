cb8bffa87804ebae577181a5e07e9dbeca62e20f
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-11-02 12:04:17 -0300

    Diseño de carrito detalles

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -109,7 +109,9 @@ function Cart() {
             d="M14 13.1V12H4.6l.6-1.1l9.2-.9L16 4H3.7L3 1H0v1h2.2l2.1 8.4L3 13v1.5c0 .8.7 1.5 1.5 1.5S6 15.3 6 14.5S5.3 13 4.5 13H12v1.5c0 .8.7 1.5 1.5 1.5s1.5-.7 1.5-1.5c0-.7-.4-1.2-1-1.4zM4 5h10.7l-1.1 4l-8.4.9L4 5z"
           />
         </svg>{" "}
-        <span className={Styles.cartLength}>{state.cart.length}</span>
+        {state.cart.length > 0 && (
+          <span className={Styles.cartLength}>{state.cart.length}</span>
+        )}
       </button>
     </div>
   );
