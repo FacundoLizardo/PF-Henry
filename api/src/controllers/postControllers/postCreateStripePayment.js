f226d420f2e4396a4a84cff86941d1494c858649
Daniel Ospina Ramirez <danielospinar@gmail.com>
2023-11-03 03:43:06 -0500

    Stripe is working now, we have Payment Gateway now it associate with the courses but have to do some validations at DB level to check some relations ships and modify and put on all tables the field enable.

---
+++ api/src/controllers/postControllers/postCreateStripePayment.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPEKEY);
+
+const postCreateStripePayment = async (req, res) => {
+	const line_items = req.body.cartItems.map((item) => {
+		return {
+			price_data: {
+				currency: "usd",
+				product_data: {
+					name: item.name,
+					images: [item.image],
+					description: item.description,
+					metadata: {
+						id: item.id,
+					},
+				},
+				unit_amount: item.price * 100,
+			},
+			quantity: 1,
+		};
+	});
+
+	const payment = await stripe.checkout.sessions.create({
+		payment_method_types: ["card"],
+		line_items,
+		mode: "payment",
+		success_url: `http://localhost:5173/payment/checkout/sucess`,
+		cancel_url: "http://localhost:5173/payment/checkout/cancel",
+	});
+
+	res.status(200).send({ url: payment.url, payment: payment.id });
+};
+
+module.exports = { postCreateStripePayment };
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -8,180 +8,176 @@ import { signOut } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import nonuser from "../../assets/navBarImages/nonuser.png";
 import Cart from "../Cart/Cart";
+import { clear } from "../../utils/clear";
 
 const Navbar = () => {
-  const [userOnSession, setuserOnSession] = useState({});
-  const userData = useContext(userContext);
+	const [userOnSession, setuserOnSession] = useState({});
+	const userData = useContext(userContext);
 
-  useEffect(() => {
-    setuserOnSession(JSON.parse(localStorage.getItem("userOnSession")));
-  }, []);
+	useEffect(() => {
+		setuserOnSession(JSON.parse(localStorage.getItem("userOnSession")));
+	}, []);
 
-  const [open, setOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [open, setOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleNavigateToInstructor = (userId) => {
-    navigate(`/instructor/${userId}`);
-  };
+	const handleNavigateToInstructor = (userId) => {
+		navigate(`/instructor/${userId}`);
+	};
 
-  const handleNavigateToStudent = (userId) => {
-    navigate(`/student/${userId}`);
-  };
+	const handleNavigateToStudent = (userId) => {
+		navigate(`/student/${userId}`);
+	};
 
-  const handleNavigateToConfig = (userId) => {
-    navigate(`/config/${userId}`);
-  };
+	const handleNavigateToConfig = (userId) => {
+		navigate(`/config/${userId}`);
+	};
 
-  const signOutFn = () => {
-    const auth = getAuth();
-    if (userOnSession) {
-      signOut(auth);
-      localStorage.removeItem("userOnSession");
-      window.location.reload();
-      setuserOnSession({});
-    } else {
-      alert("No hay ningún usuario en sesión, por favor recargue la página");
-    }
-  };
+	const signOutFn = () => {
+		const auth = getAuth();
+		if (userOnSession) {
+			signOut(auth);
+			clear();
+			setuserOnSession({});
+		} else {
+			alert("No hay ningún usuario en sesión, por favor recargue la página");
+		}
+	};
 
-  const signInFn = () => {
-    window.location = "http://localhost:5173/login";
-  };
+	const signInFn = () => {
+		window.location = "http://localhost:5173/login";
+	};
 
-  const handleMouseEnter = () => {
-    setIsModalOpen(true);
-  };
+	const handleMouseEnter = () => {
+		setIsModalOpen(true);
+	};
 
-  const handleMouseLeave = () => {
-    setIsModalOpen(false);
-  };
+	const handleMouseLeave = () => {
+		setIsModalOpen(false);
+	};
 
-  const handleClick = () => {
-    if (window.innerWidth < 768) {
-      setOpen(!open);
-    }
-  };
-  return (
-    <div className={Styles.navbarContainer}>
-      <nav>
-        <div className={Styles.navbarLogo}>
-          <img src={logo} alt="EducaStream" />
-        </div>
-        <div className={Styles.navbarAssets}>
-          <div className={`${Styles.navbarLinks} ${open ? Styles.active : ""}`}>
-            <div>
-              <ul>
-                <li>
-                  <NavLink to={"/"}>Inicio</NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/courses"}>Cursos</NavLink>
-                </li>
-                <li>
-                  {!userData ? (
-                    <NavLink to={"/login"}>Mi aprendizaje</NavLink>
-                  ) : (
-                    <NavLink to={`/student/${userData?.id}`}>
-                      Mi aprendizaje
-                    </NavLink>
-                  )}
-                </li>
-                <li>
-                  {!userData ? (
-                    <NavLink to={"/login"}>Enseñar</NavLink>
-                  ) : (
-                    <NavLink to={`/instructor/${userData?.id}`}>
-                      Enseñar
-                    </NavLink>
-                  )}
-                </li>
-                <li>
-                  <Cart />
-                </li>
-                <li
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  <div className={Styles.imgProfile}>
-                    <img src={!userData ? nonuser : userData?.photoURL} />
-                  </div>
-                  {isModalOpen &&
-                    (!userData ? (
-                      <div className={Styles.modal}>
-                        <div className={Styles.modalContent}>
-                          <button onClick={signInFn}>Ingresar</button>
-                        </div>
-                      </div>
-                    ) : (
-                      <div className={Styles.modal}>
-                        <div className={Styles.modalContent}>
-                          <button
-                            onClick={() =>
-                              handleNavigateToStudent(userData?.id)
-                            }
-                          >
-                            Mi aprendizaje
-                          </button>
-                          <button
-                            onClick={() =>
-                              handleNavigateToInstructor(userData?.id)
-                            }
-                          >
-                            Instructor
-                          </button>
-                          <hr />
-                          <button
-                            onClick={() => handleNavigateToConfig(userData?.id)}
-                          >
-                            Configuración
-                          </button>
-                          <hr />
-                          <button onClick={signOutFn}>Desconectarse</button>
-                        </div>
-                      </div>
-                    ))}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className={Styles.navbarToggle} onClick={handleClick}>
-            {open ? (
-              <svg
-                className={Styles.menuClose}
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                fill="none"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M18 6l-12 12"></path>
-                <path d="M6 6l12 12"></path>
-              </svg>
-            ) : (
-              <svg
-                className={Styles.menuOpen}
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                fill="none"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M4 6h16"></path>
-                <path d="M7 12h13"></path>
-                <path d="M10 18h10"></path>
-              </svg>
-            )}
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+	const handleClick = () => {
+		if (window.innerWidth < 768) {
+			setOpen(!open);
+		}
+	};
+	return (
+		<div className={Styles.navbarContainer}>
+			<nav>
+				<div className={Styles.navbarLogo}>
+					<img src={logo} alt="EducaStream" />
+				</div>
+				<div className={Styles.navbarAssets}>
+					<div className={`${Styles.navbarLinks} ${open ? Styles.active : ""}`}>
+						<div>
+							<ul>
+								<li>
+									<NavLink to={"/"}>Inicio</NavLink>
+								</li>
+								<li>
+									<NavLink to={"/courses"}>Cursos</NavLink>
+								</li>
+								<li>
+									{!userData ? (
+										<NavLink to={"/login"}>Mi aprendizaje</NavLink>
+									) : (
+										<NavLink to={`/student/${userData?.id}`}>
+											Mi aprendizaje
+										</NavLink>
+									)}
+								</li>
+								<li>
+									{!userData ? (
+										<NavLink to={"/login"}>Enseñar</NavLink>
+									) : (
+										<NavLink to={`/instructor/${userData?.id}`}>
+											Enseñar
+										</NavLink>
+									)}
+								</li>
+								<li>
+									<Cart />
+								</li>
+								<li
+									onMouseEnter={handleMouseEnter}
+									onMouseLeave={handleMouseLeave}>
+									<div className={Styles.imgProfile}>
+										<img src={!userData ? nonuser : userData?.photoURL} />
+									</div>
+									{isModalOpen &&
+										(!userData ? (
+											<div className={Styles.modal}>
+												<div className={Styles.modalContent}>
+													<button onClick={signInFn}>Ingresar</button>
+												</div>
+											</div>
+										) : (
+											<div className={Styles.modal}>
+												<div className={Styles.modalContent}>
+													<button
+														onClick={() =>
+															handleNavigateToStudent(userData?.id)
+														}>
+														Mi aprendizaje
+													</button>
+													<button
+														onClick={() =>
+															handleNavigateToInstructor(userData?.id)
+														}>
+														Instructor
+													</button>
+													<hr />
+													<button
+														onClick={() =>
+															handleNavigateToConfig(userData?.id)
+														}>
+														Configuración
+													</button>
+													<hr />
+													<button onClick={signOutFn}>Desconectarse</button>
+												</div>
+											</div>
+										))}
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className={Styles.navbarToggle} onClick={handleClick}>
+						{open ? (
+							<svg
+								className={Styles.menuClose}
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								fill="none">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+								<path d="M18 6l-12 12"></path>
+								<path d="M6 6l12 12"></path>
+							</svg>
+						) : (
+							<svg
+								className={Styles.menuOpen}
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								fill="none">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+								<path d="M4 6h16"></path>
+								<path d="M7 12h13"></path>
+								<path d="M10 18h10"></path>
+							</svg>
+						)}
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 };
 
 export default Navbar;
