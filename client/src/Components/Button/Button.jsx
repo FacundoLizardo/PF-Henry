81b8a26369075c49a45830329f2f4dcaabaa67ae
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-11-06 10:12:06 -0300

    Correción de filtros

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -1,9 +1,23 @@
 import Styles from "./Button.module.css";
 
 const Button = ({ text, onClick }) => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
   return (
     <div className={Styles.buttonContainer}>
-      <button className={Styles.cssButtonSharpBlue} onClick={onClick}>{text}</button>
+      <button
+        className={Styles.cssButtonSharpBlue}
+        onClick={() => {
+          onClick();
+          scrollToTop();
+        }}
+      >
+        {text}
+      </button>
     </div>
   );
 };
