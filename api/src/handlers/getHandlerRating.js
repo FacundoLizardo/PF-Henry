2c93fc458848d4fdde1f1a4eeb2344e340eec284
JoseValperga <jose.valperga@gmail.com>
2023-11-08 16:48:33 -0300

    Ruta usuarios/Cursos en proceso

--- api/src/handlers/getHandlerRating.js
+++ api/src/handlers/getHandlerRating.js
@@ -1,15 +1,5 @@
-const getCourseByRatingController = require("../controllers/getControllers/getCourseByRatingController")
-
-const getHandlerRating = async (req,res) => {
-
-    try {
-        const courseByRating = await getCourseByRatingController()
-        return res.status(200).json(courseByRating)
-        
-    } catch (error) {
-        return res.status(400).json({error: error.message})
-        
-    }
+const getHandlerRating = async (req, res) => {
+  
 };
 
 module.exports = { getHandlerRating };
