ca1442baab3f09c9d310b696fe85afa3d7ae41fb
JoseValperga <jose.valperga@gmail.com>
2023-11-10 09:17:03 -0300

    Ruta GET rating y comentario

--- api/src/routes/ratingRoutes.js
+++ api/src/routes/ratingRoutes.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const { getHandlerRating } = require("../handlers/getHandlerRating");
 const {getUserCourseHandler} = require("../handlers/getUserCourseHandler")
 const { postHandlerRating } = require("../handlers/postHandlerRating");
+const {getHandlerCourseRating} = require("../handlers/getHandlerCourseRating")
 
 const ratingRoutes = Router();
 
@@ -10,7 +11,7 @@ const ratingRoutes = Router();
 ratingRoutes.get("/userCourse", getUserCourseHandler)
 
 ratingRoutes.get("/", getHandlerRating); //Trae todos los cursos ordenados x rating
-//ratingRoutes.get("/:id",getHandlerCourseRating); //Trae rating de un curso
+ratingRoutes.get("/:id",getHandlerCourseRating); //Trae rating de un curso
 ratingRoutes.post("/create", postHandlerRating);//Crea o modifica rating y comentario
 
 
