fbe896e53fa191925ada2eb5173ee537e0c39868
Daniel Ospina Ramirez <danielospinar@gmail.com>
2023-11-09 23:55:49 -0500

    Added feature to charge courses without repeat when it was bought for error or for troll, :D will be continue tomorrow with login features and DASHBOARDgit add .!

--- api/src/controllers/getControllers/getUserController.js
+++ api/src/controllers/getControllers/getUserController.js
@@ -7,7 +7,6 @@ const getUserController = async (email) => {
 			include: [
 				{
 					model: Payment,
-					through: "UserPayment",
 					include: [
 						{
 							model: Course,
@@ -26,45 +25,40 @@ const getUserController = async (email) => {
 		if (!user) {
 			return "User Not Found";
 		}
-
-		const transformedUser = {
-			...user.toJSON(),
-			courses: user.Payments.map((payment) => ({
-				paymentId: payment.id,
-				course: payment.Courses.map((course) => course.toJSON()),
-			})),
+		return user;
+		// !Extraer la información relevante y reorganizarla (en proceso de revision, mañana 10 de nov reviso)
+		const formattedUser = {
+			id: user.id,
+			user_name: user.user_name,
+			first_name: user.first_name,
+			last_name: user.last_name,
+			name: user.name,
+			email: user.email,
+			photoURL: user.photoURL,
+			birthdate: user.birthdate,
+			role_student: user.role_student,
+			role_instructor: user.role_instructor,
+			courses: user.Payments?.reduce((courses, payment) => {
+				// !Agregar cursos pagados al array
+				courses.push(
+					...payment.courses?.map((course) => {
+						return {
+							id: course.id,
+							title: course.title,
+							description: course.description,
+							lessons: course.lesson?.map((lesson) => ({
+								id: lesson.id,
+								title: lesson.title,
+							})),
+						};
+					})
+				);
+				return courses;
+			}, []),
 		};
-
-		return transformedUser;
-		return user;
-		// const enrollments = user.Payments.map((payment) => {
-		// 	const courses = payment.Courses.map((course) => {
-		// 		return {
-		// 			id: course.id,
-		// 			title: course.title,
-		// 			price: course.price,
-		// 			description: course.description,
-		// 		};
-		// 	});
-
-		// 	return {
-		// 		paymentId: payment.id, // Agrega aquí las propiedades relevantes de los pagos si es necesario
-		// 		courses: courses,
-		// 	};
-		// });
-
-		// const userWithEnrollments = {
-		// 	user: {
-		// 		...user.dataValues,
-		// 		enrollments: enrollments,
-		// 	},
-		// };
-
-		// console.log(userWithEnrollments);
-		return user;
+		return formattedUser;
 	} catch (error) {
 		console.error(error);
-		return "Error fetching user data";
 	}
 };
 
