b0272b84b7a78390423747350cc53593092ebe40
JoseValperga <jose.valperga@gmail.com>
2023-11-10 08:53:07 -0300

    Emprolijamiento del codigo

--- api/src/handlers/getHandlerRating.js
+++ api/src/handlers/getHandlerRating.js
@@ -6,17 +6,16 @@ const getHandlerRating = async (req, res) => {
   try {
     const courses = await getAllRatingsController();
 
-    courses.forEach(curso => {
-        // Calcula el promedio de las clasificaciones
-        const totalRatings = curso.Ratings.length;
-        const sumRatings = curso.Ratings.reduce((sum, rating) => sum + rating.rating, 0);
-        const averageRating = totalRatings > 0 ? sumRatings / totalRatings : 0;
-          
-        // Asigna el promedio a la propiedad averageRating
-        curso.averageRating = averageRating;
-      
-      });
-      
+    courses.forEach((curso) => {
+      const totalRatings = curso.Ratings.length;
+      const sumRatings = curso.Ratings.reduce(
+        (sum, rating) => sum + rating.rating,
+        0
+      );
+      const averageRating = totalRatings > 0 ? sumRatings / totalRatings : 0;
+
+      curso.averageRating = averageRating;
+    });
 
     return res.status(200).json(courses);
   } catch (error) {
