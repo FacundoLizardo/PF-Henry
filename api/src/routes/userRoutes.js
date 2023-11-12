aa920eb9aa0b60488ee030502ceb149c275f2007
Daniel Ospina Ramirez <danielospinar@gmail.com>
2023-11-12 16:03:42 -0500

    Get All Users added

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 const { postHandlerUser } = require("../handlers/postHandlerUser");
 const { getHandlerUser } = require("../handlers/getHandlerUser");
 const { putHandlerUser } = require("../handlers/putHandlerUser");
+const { getHandlerAllUser } = require("../handlers/getHandlerAllUsers");
 
 const userRoutes = Router();
 userRoutes.post("/create", postHandlerUser);
 userRoutes.get("/user", getHandlerUser);
 userRoutes.put("/user/edit", putHandlerUser);
-
+userRoutes.get("/all", getHandlerAllUser);
 module.exports = userRoutes;
